8944769b0cbce815dfc22fc3a98550c5fb96bc30
pratyay mustafi <pratyaymustafi@outlook.com>
2026-08-02 19:15:23 +0530

    api

---
+++ .railway/railway.ts
@@ -0,0 +1,13 @@
+import { defineRailway, project, service } from "railway/iac";
+
+export default defineRailway(() => {
+  const app = service("searchapi", {
+    host: "0.0.0.0",
+    port: "8000",
+    image: "ghcr.io/pratyay360/searchapi:latest",
+  });
+
+  return project("searchapi", {
+    resources: [app],
+  });
+});
